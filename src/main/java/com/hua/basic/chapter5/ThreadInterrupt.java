18780b06f1a302602ea4390c01f9259372e66f76
ccx_huang <1311607595@qq.com>
2019-09-06 16:50:55 +0800

    9.5 多线程 thread interrupt

--- src/main/java/com/hua/basic/chapter5/ThreadInterrupt.java
+++ src/main/java/com/hua/basic/chapter5/ThreadInterrupt.java
@@ -5,7 +5,26 @@ package com.hua.basic.chapter5;
  * @date 2019/9/5 15:21
  */
 public class ThreadInterrupt {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
+        Thread t = new Thread(){
+            @Override
+            public void run() {
+                while (true) {
+                    try {
+                        Thread.sleep(10);
+                    } catch (InterruptedException e) {
+                        System.out.println("收到打断信号");
+                        e.printStackTrace();
+                    }
+                }
+            }
+        };
+
+        t.start();
+        Thread.sleep(100);
+        System.out.println(t.isInterrupted());
+        t.interrupt();
+        System.out.println(t.isInterrupted());
 
     }
 }
