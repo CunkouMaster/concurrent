a9d5660424057817c50598b9d2fccf39313b459f
huazai <1311607595@qq.com>
2019-10-08 21:01:00 +0800

    Signed-off-by: huazai <1311607595@qq.com>

--- src/main/java/com/hua/basic/chapter9/ExitCapture.java
+++ src/main/java/com/hua/basic/chapter9/ExitCapture.java
@@ -5,4 +5,40 @@ package com.hua.basic.chapter9;
  * @date 2019/10/2 15:04
  */
 public class ExitCapture {
+    public static void main(String[] args) {
+        Runtime.getRuntime().addShutdownHook(new Thread(() -> {
+            System.out.println("the application will be exit");
+            notifyAndRelease();
+        }));
+        int i = 0;
+        while (true) {
+            try {
+                Thread.sleep(1000);
+                System.out.println("I am working");
+            } catch (Throwable e) {
+                //ignore
+            }
+            i++;
+            if(i>6){
+                throw new RuntimeException("error");
+            }
+        }
+    }
+
+    private static void notifyAndRelease() {
+        System.out.println("notify to the admin");
+        try {
+            Thread.sleep(1000);
+        } catch (Throwable e) {
+            //ignore
+        }
+        System.out.println("will release resource");
+        try {
+            Thread.sleep(1000);
+        } catch (Throwable e) {
+            //ignore
+        }
+        System.out.println("release and notify done");
+    }
+
 }
--- src/main/java/com/hua/basic/chapter9/ThreadException.java
+++ src/main/java/com/hua/basic/chapter9/ThreadException.java
@@ -4,5 +4,5 @@ package com.hua.basic.chapter9;
  * @author huazaiqd
  * @date 2019/10/2 15:23
  */
-public class ThreadExecption {
+public class ThreadException {
 }
