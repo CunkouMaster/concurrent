7a5d6ae66683942dad931e1fe02e3c8fbfd47996
黄皓华 <huazaiqd@gmail.com>
2019-12-01 19:55:12 +0800

    12.1 count down

---
+++ src/main/java/com/hua/designPattern/countDown_13/JDKCountDown.java
@@ -0,0 +1,19 @@
+package com.hua.designPattern.countDown_13;
+
+import java.util.Random;
+
+/**
+ * @author huazai
+ * @date 2019-11-30 11:17
+ */
+public class JDKCountDown {
+
+    private static final Random random = new Random(System.currentTimeMillis());
+
+    public static void main(String[] args) {
+        System.out.println("");
+
+        
+    }
+
+}
